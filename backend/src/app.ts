c44ba8e39d05cfb0e251a2f48e76331568121f42
AnastasiaS99 <kuznetzova.a2013@yandex.ru>
2026-07-18 12:03:29 +0300

    Первый комит

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -15,7 +15,10 @@ const app = express()
 
 app.use(cookieParser())
 
-app.use(cors())
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+}))
 // app.use(cors({ origin: ORIGIN_ALLOW, credentials: true }));
 // app.use(express.static(path.join(__dirname, 'public')));
 
