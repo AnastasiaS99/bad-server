43bf64d8a88e841f689215bd4876076c74fa8d5d
AnastasiaS99 <kuznetzova.a2013@yandex.ru>
2026-07-23 21:21:47 +0300

    Пятая попытка

--- backend/src/middlewares/rate-limiter.ts
+++ backend/src/middlewares/rate-limiter.ts
@@ -2,7 +2,7 @@ import rateLimit from "express-rate-limit";
 
 export const limiter = rateLimit({
     windowMs: 1000, // 1 секунда
-    limit: 5, // 5 запросов в секунду
+    limit: 3, // 3 запроса в секунду (еще строже)
     standardHeaders: true,
     legacyHeaders: false,
     skip: (req) => req.path === '/auth/csrf-token',
